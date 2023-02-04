c12f309d04167a7aae628d964c3d87f772b97a34
KimlongHor <kimlonghor11@gmail.com>
2023-02-04 19:57:32 +0000

    First commit

---
+++ WS1/1.16Lab.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	printf("  NO PARKING\n2:00 - 6:00 am.\n");
+	return 0;
+}
---
+++ WS1/1.18Lab.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+	printf("Output 1:\n");
+	printf("Hello world!\n\n");
+
+	printf("Output 2:\n");
+	printf("Hello world! \nHow are you?\n\n");
+
+	printf("Output 3:\n");
+	printf("Hello world! \nHow are you?\n   (I'm fine).\n");
+
+	return 0;
+}
