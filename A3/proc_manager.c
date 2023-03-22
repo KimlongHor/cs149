a73f44efd20c6b8e8cc5fe4e55e83c9dbfe2667a
KimlongHor <kimlonghor11@gmail.com>
2023-03-22 06:07:33 +0000

    Create files and write outputs from child processes

--- A3/proc_manager.c
+++ A3/proc_manager.c
@@ -6,8 +6,12 @@
  * Creation date: 03/21/2023
  **/
  
- #include <stdio.h>
- #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <fcntl.h>
  
  #define MAX_NUM_COMMAND 100
  #define MAX_LENGTH 30
@@ -15,6 +19,7 @@
  int main(void) {
  	char command[MAX_NUM_COMMAND][MAX_LENGTH];
  	char numOfCommand = 0;
+ 	int pid = 0;
  	
  	while (fgets(command[numOfCommand], MAX_LENGTH, stdin)) {
  		
@@ -27,7 +32,45 @@
  	}
  	
  	for(int i = 0; i < numOfCommand; i++) {
- 		printf("%s\n", command[i]);
+ 		pid = fork();
+ 		
+ 		if (pid < 0) {
+ 			fprintf(stderr, "Failed to fork");
+ 			exit(1);
+ 		} else if (pid == 0) {
+ 			char outputFile[100];
+ 			char errorFile[100];
+ 			
+ 			sprintf(outputFile, "%d.out", getpid());
+ 			sprintf(errorFile, "%d.out", getpid());
+ 			
+ 			int outputFd = open(outputFile, O_RDWR | O_CREAT | O_APPEND, 0777);
+ 			int errorFd = open(errorFile, O_RDWR | O_CREAT | O_APPEND, 0777);
+ 			
+ 			if (outputFd < 0) {
+ 				perror("Failed opening output file.");
+ 				exit(1);
+ 			}
+ 			
+ 			if (errorFd < 0) {
+ 				perror("Failed opening error file.");
+ 				exit(1);
+ 			}
+ 			
+ 			if(dup2(outputFd, 1) < 0) {
+ 				perror("Failed dup2 for outputFd");
+ 				exit(2);
+ 			}
+ 			close(outputFd);
+ 			
+ 			if(dup2(errorFd, 2) < 0) {
+ 				perror("Failed dup2 for errorFd");
+ 				exit(2);
+ 			}
+ 			close(errorFd);
+ 			
+ 			printf("Starting command %d: child %d pid of parent %d", i, getpid(), getppid());		
+ 		}
  	}
  	
  	return 0;
