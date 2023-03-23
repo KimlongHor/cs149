bd28ef0bff74633ea51a002478d829913db60528
KimlongHor <kimlonghor11@gmail.com>
2023-03-23 04:40:23 +0000

    Add comments

--- A3/proc_manager.c
+++ A3/proc_manager.c
@@ -1,5 +1,5 @@
 /**
- * Description: This program 
+ * Description: This program takes multiple commands and execute them using execvp in child processes.
  * Author names: Kimlong Hor, Jimmy Phan
  * Author emails: kimlong.hor@sjsu.edu, jimmy.phan@sjsu.edu
  * Last modified date: 03/21/2023
@@ -21,9 +21,10 @@
  	char numOfCommand = 0;
  	int pid = 0;
  	
+ 	// This while loop read all command line until control-d and store them in an array
  	while (fgets(command[numOfCommand], MAX_LENGTH, stdin)) {
  		
- 		// replace "\n" at the end of the name with "\0"
+ 		// replace "\n" at the end of the command with "\0"
  		if (command[numOfCommand][strlen(command[numOfCommand]) - 1] == '\n') {
  			command[numOfCommand][strlen(command[numOfCommand]) - 1] = '\0';
  		}
@@ -31,19 +32,24 @@
  		numOfCommand++;
  	}
  	
+ 	// This loops through each command of the array
  	for(int i = 0; i < numOfCommand; i++) {
+ 	
+ 		// create a child process
  		pid = fork();
  		
- 		if (pid < 0) {
+ 		if (pid < 0) { // if fork() fails
  			fprintf(stderr, "Failed to fork");
  			exit(1);
- 		} else if (pid == 0) {
+ 		} else if (pid == 0) { // child process
  			char outputFile[100];
  			char errorFile[100];
  			
+ 			// set up two file names; one for output and one for error
  			sprintf(outputFile, "%d.out", getpid());
  			sprintf(errorFile, "%d.err", getpid());
  			
+ 			// create file descriptor for output and error files
  			int outputFd = open(outputFile, O_RDWR | O_CREAT | O_APPEND, 0777);
  			int errorFd = open(errorFile, O_RDWR | O_CREAT | O_APPEND, 0777);
  			
@@ -57,17 +63,21 @@
  				exit(1);
  			}
  			
+ 			// duplicate outputFd into stdout which is 1
  			if(dup2(outputFd, 1) < 0) {
  				perror("Failed dup2 for outputFd");
  				exit(2);
  			}
  			
+ 			// duplicate errorFd into stderr which is 2
  			if(dup2(errorFd, 2) < 0) {
  				perror("Failed dup2 for errorFd");
  				exit(2);
  			}
  
  			printf("Starting command %d: child %d pid of parent %d\n", i+1, getpid(), getppid());
+ 			
+ 			// clean buffer. Without this, the [pid].out will be overwritten by the execvp result
  			fflush(STDIN_FILENO);
  			
  			close(outputFd);
@@ -85,10 +95,7 @@
  			
  			arg[argCount+1] = NULL;
  			
- 			for(int i = 0; i <= argCount; i++) {
- 				printf("HERE: %d => %s", i, arg[i]);
- 			}
- 			
+ 			// execute the command using execvp
  			if (execvp(arg[0], arg) < 0) {
  				perror("Failed execvp");
  				exit(2);
@@ -103,14 +110,18 @@
  		char outputFile[100];
 		char errorFile[100];
 		
+		// set up two file names; one for output and one for error
 		sprintf(outputFile, "%d.out", pid);
 		sprintf(errorFile, "%d.err", pid);
 		
+		// open both output and error files; "a" is append
 		FILE *outFile = fopen(outputFile, "a");
 		FILE *errFile = fopen(errorFile, "a");
 		
+		// write a message into output file
 		fprintf(outFile, "Finish child %d pid of parent %d\n", pid, getpid());
 		
+		// check the status and write a message to error file accordingly
 		if (WIFEXITED(status))
         	{
             		fprintf(errFile, "Exited with exitcode = %d", WEXITSTATUS(status));
